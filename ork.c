f3b18bb234e40ce27558d98306a271291a1263dd
corwin <corwin@ubuntu>
2014-03-10 22:57:19 -0700

    created source files

---
+++ ork.c
@@ -0,0 +1,43 @@
+#include "Ork.h"
+
+int main (int argc, char *argv[])
+{
+init()
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+cleanup()
+}
+
+int init ()
+{
+    SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow(
+        "An SDL2 window",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+
+    if (window == NULL) {
+        // In the event that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    );
+
+
+}
+
+int cleanup ()
+{
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+}
+
---
+++ ork.h
@@ -0,0 +1,12 @@
+#ifndef _ORK_H_
+    #define _ORK_H_
+//ork stands for Origami Knight
+#include "SDL.h"
+#include <stdio.h>
+#include <stdlib.h>
+//including all things
+
+int init ();
+int cleanup ();
+ 
+#endif
